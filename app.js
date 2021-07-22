235aed47b61972defe9736dc802d741c42887520
webroot-ua <86402619+webroot-ua@users.noreply.github.com>
2021-07-22 12:03:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use('/t/', require('./routes/redirect.routes'))
 if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-    ap.get('*', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
